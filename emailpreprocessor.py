2b569f4f1fc0837ec27808f9aa2df5e102708e7b
Chris Dailey <christopher.dailey@gmail.com>
2015-04-02 19:28:44 -0400

    epp update

--- emailpreprocessor.py
+++ emailpreprocessor.py
@@ -1,10 +1,13 @@
 from os import listdir
 import re
 
-keptFiles = []
-keptRE = re.compile("edrm-enron-v2_.*-._xml\.zip")
+def main(argv):
+	keptFiles = []
+	keptRE = re.compile("edrm-enron-v2_.*-._xml\.zip")
 
-for filename in os.lisdir():
-	if (keptRE.match(filename):
-		print filename
+	for filename in os.lisdir(argv[0]):
+		if (keptRE.match(filename):
+			print filename
 	
+if __name__ == "__main__": 
+    main(sys.argv) 
