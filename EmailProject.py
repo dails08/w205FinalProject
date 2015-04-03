338b4d64eb040568e4c41247df28ad9f4c96fcf0
Chris Dailey <christopher.dailey@gmail.com>
2015-04-03 15:40:06 -0400

    updated preprocessor

--- EmailProject.py
+++ EmailProject.py
@@ -3,23 +3,6 @@ import re
 import sys
 
 
-#def main(argv): 
-	#dateRE = re.compile("^Date: .*")
-	#subjectRE = re.compile("^Subject: .*")
-	#fromRE = re.compile("^From: .*")
-	#toRE = re.compile("^To: .*")
-	#origMessageRE = re.compile("-----Original Message-----")
-	#emailAddressRE = re.compile("<[^@]+@[^@]+\.[^@]+>")    
-	#for line in sys.stdin: 
-		#if (fromRE.match(line)):
-			#if (emailAddressRE.search(line)):
-				#addy = emailAddressRE.search(line).group(0)
-				#if (len(addy) < 50):
-					#print "LongValueSum:" + addy.lower() + "\t" + "1"
-#if __name__ == "__main__": 
-    #main(sys.argv) 
-
-
 def main(argv): 
 	dateRE = re.compile("^Date: .*")
 	subjectRE = re.compile("^Subject: .*")
