4414450fc5329b32c06598796bee8f7fc1c791df
Chris Dailey <christopher.dailey@gmail.com>
2015-04-03 16:57:44 -0400

    preprocessor recognizes and extracts zip files

--- emailpreprocessor.py
+++ emailpreprocessor.py
@@ -1,17 +1,29 @@
-from os import listdir
+import os
 import re
-
+import sys
+from subprocess import call
+import zipfile
 
 def main(argv):
 	keptFiles = []
-	keptRE = re.compile("edrm-enron-v2_.*-._xml\.zip")
-
-	for filename in os.lisdir(argv[0]):
+	keptRE = re.compile("edrm-enron-v2_.*-.*_xml\.zip")
+	
+	baseDir = argv[1]
+	
+	for filename in os.listdir(baseDir):
 		if (keptRE.match(filename)):
-			keptFiles.extend(filename)
+			keptFiles.append(filename)
 			print filename
 			
-	#for filename in keptFiles:
+	for filename in keptFiles:
+		print filename
+		z = zipfile.ZipFile(filename, 'r')
+		shortname = re.match("edrm-enron-v2_(.*-.*)_xml\.zip", filename).group(1)
+		if not os.path.exists(baseDir + "/" + shortname):
+			os.makedirs(baseDir + "/" + shortname)
+		z.extractall(baseDir + "/" + shortname)
+		
+		
 		
 	
 if __name__ == "__main__": 
