4c37febf68dc632c30b55cd4bf2485f7455d9d1a
Chris Dailey <christopher.dailey@gmail.com>
2015-04-03 23:02:42 -0400

    Updating the accuracy of email extraction

--- EmailProject.py
+++ EmailProject.py
@@ -9,14 +9,14 @@ def main(argv):
 	fromRE = re.compile("^From: .*")
 	toRE = re.compile("^To: .*")
 	origMessageRE = re.compile("-----Original Message-----")
-	emailAddressRE = re.compile("<[^@]+@[^@]+\.[^@]+>")    
+	emailAddressRE = re.compile("<([^@]+@[^@]+\.[^@]+)>")    
 	while True:
 		s = sys.stdin.readline()
 		if not s:
 			break
 		if (fromRE.match(s)):
 			if (emailAddressRE.search(s)):
-				addy = emailAddressRE.search(s).group(0)
+				addy = emailAddressRE.search(s).group(1)
 				if (len(addy) < 50):
 					print "LongValueSum:" + addy.lower() + "\t" + "1"
 if __name__ == "__main__": 
