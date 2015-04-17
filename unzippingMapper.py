36b951c35f22be1a0e9ad397be4e73074219a94c
Chris Dailey <christopher.dailey@gmail.com>
2015-04-17 17:45:49 -0400

    trying ec2 isntance credential usage for mapper

--- unzippingMapper.py
+++ unzippingMapper.py
@@ -5,6 +5,8 @@ import boto
 import zipfile
 from boto.s3.key import Key
 from boto.s3.connection import S3Connection
+from boto.utils import get_instance_metadata
+from boto.sts import STSConnection
 
 
 #read from stdin
@@ -16,7 +18,27 @@ from boto.s3.connection import S3Connection
 
 def main(argv):
 	
+	#print "started"
 	
+	#establish instance credentials
+	
+	#instanceMD = boto.utils.get_instance_metadata()
+	
+	#accesKey = instanceMD['iam']['security-credentials']['get-enron-zips']['AccessKeyId']
+	#secretKey = instanceMD['iam']['security-credentials']['get-enron-zips']['SecretAccessKey']
+	
+	#stsconn = boto.sts.STSConnection(accesKey, secretKey)
+	
+	
+	#sts_connection = STSConnection()
+	#assumedRoleObject = sts_connection.assume_role(role_arn="arn:aws:iam::321504313022:role/get-enron-zips", role_session_name="AssumeRoleSession1")
+
+	# Use the temporary credentials returned by AssumeRole to call Amazon S3  
+	# and list all buckets in the account that owns the role (the trusting account)
+	#print "Connecting"
+	#s3conn = S3Connection(aws_access_key_id=assumedRoleObject.credentials.access_key, aws_secret_access_key=assumedRoleObject.credentials.secret_key, security_token=assumedRoleObject.credentials.session_token)
+	s3conn = S3Connection()
+	#print "Connected"
 	
 	dateRE = re.compile("^Date: .*")
 	subjectRE = re.compile("^Subject: .*")
@@ -24,26 +46,34 @@ def main(argv):
 	toRE = re.compile("^To: .*")
 	origMessageRE = re.compile("-----Original Message-----")
 	emailAddressRE = re.compile("<([^@]+@[^@]+\.[^@]+)>") 
+	
 	while True:
 		s = sys.stdin.readline()
 		if not s:
 			break
 		#download
-		conn = boto.connect_s3()
-		zipbucket = conn.get_bucket("chrisdailey1-enron")
+
+		zipbucket = s3conn.get_bucket("chrisdailey1-enron")
+		#print "Got bucket"
 		
 		k = Key(zipbucket)
 		k.key = "zips/" + s
+		#print "Saving"
 		k.get_contents_to_filename(s)
+		#print "Saved"
 		try:
 			z = zipfile.ZipFile(s, 'r')
 		except zipfile.BadZipfile as e:
 			print e
 			continue
 		shortname = re.match("edrm-enron-v2_(.*-.*)_xml\.zip", s).group(1)
+		#print "making directory"
 		if not os.path.exists(shortname):
 			os.makedirs(shortname)
+		#print "Made"
+		#print "Extracting"
 		z.extractall(shortname + "/")
+		#print "Extracted"
 		#the email unzip into a couple of different folder, one of which stores
 		#only the text of the email.  I think I've seen folders with more than one text
 		#folder, so go through them one by one and if they match the name format
@@ -56,13 +86,17 @@ def main(argv):
 				#go through each text file
 				for textFile in os.listdir(textDirectory):
 					#read it
+					#print "Opening textfile"
 					emailText = open(textDirectory + "/" + textFile, 'r')
+					#print "Opened"
 					#emit keys
 					if (fromRE.match(s)):
 						if (emailAddressRE.search(s)):
 							addy = emailAddressRE.search(s).group(1)
 							if (len(addy) < 50):
+								#print "Emitting"
 								print "LongValueSum:" + addy.lower() + "\t" + "1"
+								#print "Emitted"
 					#close it
 					emailText.close()
 
@@ -70,7 +104,9 @@ def main(argv):
 		#delete their special folder since it's not needed anymore
 		#if we leave it we'll take up a ton of space (presumably)
 		#with everyone's unzipped emails
+		#print "Deleting"
 		shutil.rmtree(shortname+"/")
+		#print "Deleted"
 		
 		
 		
